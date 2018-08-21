0739a23d43a7e7a957dfe7a651342a3ce3626f39
Muhammad Usman Sharif <sh4rif@hotmail.co.uk>
2018-08-21 11:33:42 +0100

    Added Mustache.js for message template

--- public/js/index.js
+++ public/js/index.js
@@ -10,22 +10,25 @@ socket.on("disconnect", () => {
 
 socket.on("newMessage", message => {
   var formattedTime = moment(message.createdAt).format("h:mm:ss a");
-  var li = $("<li></li>");
-  li.text(`${message.from} (${formattedTime}): ${message.text}`);
-
-  $("#messages").append(li);
+  var template = $("#message-template").html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+  $("#messages").append(html);
 });
 
 socket.on("newLocationMessage", message => {
-  var li = $("<li></li>");
-  var a = $("<a target='_blank'>My current location</a>");
   var formattedTime = moment(message.createdAt).format("h:mm a");
+  var template = $("#location-message-template").html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    createdAt: formattedTime,
+    url: message.url
+  });
 
-  li.text(`${message.from} ${formattedTime}:`);
-  a.attr("href", message.url);
-  li.append(a);
-
-  $("#messages").append(li);
+  $("#messages").append(html);
 });
 
 $("#message-form").on("submit", event => {
